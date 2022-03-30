935ee8136a000d90d83357fb921b01379d944457
William May <dual.william.may@gmail.com>
2022-03-30 12:17:44 -0600

    Actualizar agregado

--- productos/Form1.Designer.cs
+++ productos/Form1.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.primaryGrid = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.actualizar = new System.Windows.Forms.Button();
             this.eliminar = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -54,14 +55,14 @@
             this.primaryGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
             this.primaryGrid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
             this.primaryGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.primaryGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.primaryGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.primaryGrid.ColumnHeadersHeight = 30;
             this.primaryGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.primaryGrid.EnableHeadersVisualStyles = false;
@@ -69,33 +70,53 @@
             this.primaryGrid.Location = new System.Drawing.Point(0, 0);
             this.primaryGrid.Name = "primaryGrid";
             this.primaryGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.primaryGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.primaryGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.primaryGrid.RowHeadersVisible = false;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.White;
-            this.primaryGrid.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            this.primaryGrid.RowsDefaultCellStyle = dataGridViewCellStyle3;
             this.primaryGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.primaryGrid.Size = new System.Drawing.Size(576, 308);
+            this.primaryGrid.Size = new System.Drawing.Size(576, 281);
             this.primaryGrid.TabIndex = 4;
+            this.primaryGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.actualizar);
             this.panel1.Controls.Add(this.eliminar);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.primaryGrid);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(579, 371);
+            this.panel1.Size = new System.Drawing.Size(579, 358);
             this.panel1.TabIndex = 5;
+            // 
+            // actualizar
+            // 
+            this.actualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            this.actualizar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.actualizar.FlatAppearance.BorderSize = 2;
+            this.actualizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.actualizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.actualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.actualizar.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actualizar.ForeColor = System.Drawing.Color.White;
+            this.actualizar.Location = new System.Drawing.Point(416, 304);
+            this.actualizar.Name = "actualizar";
+            this.actualizar.Size = new System.Drawing.Size(146, 37);
+            this.actualizar.TabIndex = 16;
+            this.actualizar.Text = "Actualizar";
+            this.actualizar.UseVisualStyleBackColor = false;
+            this.actualizar.Click += new System.EventHandler(this.actualizar_Click);
             // 
             // eliminar
             // 
@@ -107,7 +128,7 @@
             this.eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.eliminar.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.eliminar.ForeColor = System.Drawing.Color.White;
-            this.eliminar.Location = new System.Drawing.Point(185, 322);
+            this.eliminar.Location = new System.Drawing.Point(183, 304);
             this.eliminar.Name = "eliminar";
             this.eliminar.Size = new System.Drawing.Size(146, 37);
             this.eliminar.TabIndex = 15;
@@ -125,7 +146,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(12, 322);
+            this.button1.Location = new System.Drawing.Point(12, 304);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(146, 37);
             this.button1.TabIndex = 14;
@@ -138,7 +159,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(616, 50);
+            this.label1.Location = new System.Drawing.Point(610, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 20);
             this.label1.TabIndex = 6;
@@ -147,7 +168,7 @@
             // nombre
             // 
             this.nombre.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nombre.Location = new System.Drawing.Point(702, 42);
+            this.nombre.Location = new System.Drawing.Point(696, 24);
             this.nombre.Name = "nombre";
             this.nombre.Size = new System.Drawing.Size(207, 28);
             this.nombre.TabIndex = 7;
@@ -158,7 +179,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(616, 118);
+            this.label2.Location = new System.Drawing.Point(610, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 20);
             this.label2.TabIndex = 8;
@@ -167,7 +188,7 @@
             // codigo
             // 
             this.codigo.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.codigo.Location = new System.Drawing.Point(702, 110);
+            this.codigo.Location = new System.Drawing.Point(696, 84);
             this.codigo.Name = "codigo";
             this.codigo.Size = new System.Drawing.Size(207, 28);
             this.codigo.TabIndex = 9;
@@ -178,7 +199,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(616, 186);
+            this.label3.Location = new System.Drawing.Point(610, 160);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 20);
             this.label3.TabIndex = 10;
@@ -187,7 +208,7 @@
             // categoria
             // 
             this.categoria.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.categoria.Location = new System.Drawing.Point(702, 178);
+            this.categoria.Location = new System.Drawing.Point(696, 152);
             this.categoria.Name = "categoria";
             this.categoria.Size = new System.Drawing.Size(207, 28);
             this.categoria.TabIndex = 11;
@@ -198,7 +219,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(616, 256);
+            this.label4.Location = new System.Drawing.Point(610, 230);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(70, 20);
             this.label4.TabIndex = 12;
@@ -207,7 +228,7 @@
             // precio
             // 
             this.precio.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.precio.Location = new System.Drawing.Point(702, 248);
+            this.precio.Location = new System.Drawing.Point(696, 222);
             this.precio.Name = "precio";
             this.precio.Size = new System.Drawing.Size(207, 28);
             this.precio.TabIndex = 13;
@@ -218,7 +239,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(60)))));
-            this.ClientSize = new System.Drawing.Size(921, 371);
+            this.ClientSize = new System.Drawing.Size(921, 358);
             this.Controls.Add(this.precio);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.categoria);
@@ -252,6 +273,7 @@
         private System.Windows.Forms.TextBox precio;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button eliminar;
+        private System.Windows.Forms.Button actualizar;
 
     }
 }
--- productos/Form1.cs
+++ productos/Form1.cs
@@ -24,7 +24,7 @@ namespace productos
         
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            con.mostrar("productos", primaryGrid);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -78,6 +78,49 @@ namespace productos
                 {
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("Se ha eliminado el registro");
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    throw;
+                }
+            }
+            con.mostrar("productos", primaryGrid);
+            conectar.Close();
+        }
+
+        public int filaActual()
+        {
+            int index = primaryGrid.CurrentRow.Index;
+            return index;
+        }
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nombre.Text = primaryGrid.Rows[filaActual()].Cells["nombre"].Value.ToString();
+            codigo.Text = primaryGrid.Rows[filaActual()].Cells["codigo"].Value.ToString();
+            categoria.Text = primaryGrid.Rows[filaActual()].Cells["categoria"].Value.ToString();
+            precio.Text = primaryGrid.Rows[filaActual()].Cells["precio"].Value.ToString();
+        }
+        private void actualizar_Click(object sender, EventArgs e)
+        {
+            string id = primaryGrid.Rows[filaActual()].Cells["idProducto"].Value.ToString();
+            conectar.ConnectionString = server;
+            conectar.Open();
+            SqlCommand cmd = new SqlCommand("SP_INSERTUPDATEDELETE", conectar);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@IDPRODUCTO", id);
+            cmd.Parameters.AddWithValue("@NOMBRE", nombre.Text);
+            cmd.Parameters.AddWithValue("@CODIGO", codigo.Text);
+            cmd.Parameters.AddWithValue("@CATEGORIA", categoria.Text);
+            cmd.Parameters.AddWithValue("@PRECIO", precio.Text);
+            cmd.Parameters.AddWithValue("@StatementType", "Update");
+
+            if (MessageBox.Show("¿Realmente dese cambiar estos datos?", "Advertencia", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto Actualizado");
                 }
                 catch (SqlException ex)
                 {
